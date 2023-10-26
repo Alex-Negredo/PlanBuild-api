5f5e26f12233ade0a16c576ead61401af4e6be33
Alex Negredo <alexandrenegredo@gmail.com>
2023-10-25 22:45:27 -0700

    changing the port to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
+const port = 3000;
 const multer = require('multer');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
